f459365087475fb489e83a89b28161188f5ecf7e
shashikant singh <114600939+Shashikant32@users.noreply.github.com>
2024-11-15 11:30:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page Loaded. Ready for form interaction.");
+    
+    document.querySelector("form").addEventListener("submit", function(event) {
+        if (!document.getElementById('tos').checked) {
+            alert("You must agree to the Terms of Service.");
+            event.preventDefault(); 
+        }
+    });
+});
